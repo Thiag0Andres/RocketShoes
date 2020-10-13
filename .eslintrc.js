b69f381a1666571a1c4ddd14b9c2e2cc6ebed837
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-10-13 14:09:33 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+   env: {
+      browser: true,
+      es2021: true,
+   },
+   extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'prettier',
+      'prettier/react',
+   ],
+   parser: ['@typescript-eslint/parser', 'babel-eslint'],
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+   },
+   plugins: ['react', '@typescript-eslint', 'prettier'],
+   rules: {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+      'import/prefer-default-export': 'off',
+   },
+};
